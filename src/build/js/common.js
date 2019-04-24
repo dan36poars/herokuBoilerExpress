eb216287219d44d0656d27a2fc473cbe550056c9
Daniel Corrêa <eldestemido@gmail.com>
2019-04-24 08:03:15 -0300

    Ready for Production

--- src/build/js/common.js
+++ src/build/js/common.js
@@ -1,2 +1,4 @@
 import 'core-js/fn/promise';
-import 'lodash';
+import 'core-js/fn/array';
+import 'core-js/es7/array';
+// import 'lodash';
--- src/build/js/index.bundle.js
+++ src/build/js/index.bundle.js
@@ -2,3 +2,13 @@ import './common';
 import '../scss/index.scss';
 var d3 = require('d3');
 var utility2 = require('./utility2');
+
+let A = [ 1, 3, 5 , 76, 47 ];
+let B = [ 3 , 4 ];
+
+A.forEach( function (element) {
+  if ( B.includes( element ) ) {
+    console.log( element );
+  }
+});
+
